4d66aa540706b5f9b9a9fe2c599fdcd5ac4c6779
begota98 <begota98@gmail.com>
2021-01-10 19:32:23 +0100

    upiti vezani za korisnika

--- Aplikacija/Uno Infinity War/server.ts
+++ Aplikacija/Uno Infinity War/server.ts
@@ -6,6 +6,14 @@ import path from "path";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import http from "http";
+import {
+  igracModel,
+  kartaModel,
+  chatModel,
+  korisnikModel,
+  igraModel,
+  odigranaKartaModel,
+} from "./model/ModelBazePodataka";
 
 dotenv.config();
 
@@ -25,3 +33,30 @@ app.use(cors());
 server.listen(process.env.PORT || PORT, () => {
   console.log(`Server osluskuje na portu : ${process.env.PORT || PORT}`);
 });
+
+app.get("/vratikorisnike", async function (req, res) {
+  let korisnici = await korisnikModel.find();
+  console.log(korisnici);
+  res.json(korisnici);
+});
+
+//http://localhost:3000/vratikorisnike
+
+app.get("/vratikorisnika/:id", async function (req, res) {
+  let korisnik = await korisnikModel.findById(req.params.id);
+  console.log(korisnik);
+  res.json(korisnik);
+});
+//http://localhost:3000/vratikorisnika/BinFicP2ttfws0JCei2kMPcL0xY2
+//BinFicP2ttfws0JCei2kMPcL0xY2
+
+app.get("/vratiigrekorisnika/:id", async function (req, res) {
+  let korisnik = await korisnikModel.findById(req.params.id);
+  if (korisnik) {
+    console.log(korisnik.get("prethodneIgre"));
+    res.json(korisnik.get("prethodneIgre"));
+  } else {
+    res.json("");
+  }
+});
+//http://localhost:3000/vratiigrekorisnika/BinFicP2ttfws0JCei2kMPcL0xY2
