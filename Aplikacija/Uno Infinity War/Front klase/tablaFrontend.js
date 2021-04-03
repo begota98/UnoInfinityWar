ec22562449a2f4bdbcd5d62aa50221e473900c09
begota98 <begota98@gmail.com>
2021-04-03 22:48:46 +0200

    Stranica za pregled neke prethodne igre i pomocna klasa za front prikaz elemenata table

---
+++ Aplikacija/Uno Infinity War/Front klase/tablaFrontend.js
@@ -0,0 +1,34 @@
+
+
+var TablaFrontend = function (trenutnaKarta, dodatnaKlasa)
+{
+  
+    this.trenutnaKarta = trenutnaKarta;
+    this.dodatnaKlasa = dodatnaKlasa;
+  
+
+  this.renderujTablu = function(destination) 
+  {
+    let parent = destination;
+    let div = document.createElement("div");
+    div.className = " board-card ";
+    div.className += this.dodatnaKlasa ? this.dodatnaKlasa : "";
+    parent.appendChild(div);
+    let karta = new KartaFrontend(this.trenutnaKarta,0);
+    parent = div;
+    
+    let img = document.createElement("img");
+    parent.appendChild(img);
+    console.log(this.trenutnaKarta);
+    img.dataset.color = this.trenutnaKarta.boja;
+    img.dataset.value = this.trenutnaKarta.vrednost;
+    img.dataset.isSpecial = this.trenutnaKarta.specijalna ? 1 : 0;
+    img.dataset.index = this.index;
+    karta.odredjivanjeImenaFajla();
+    img.className = "card-deck";
+    img.src = "Slike_spil/" + karta.imefajla;
+   
+  }
+}
+
+
